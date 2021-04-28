118feb432c8b759db367076ff80f8c6f3f52761d
Junrae Kim <jrk9204@gmail.com>
2021-04-28 14:23:22 +0900

    merge package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom'
->>>>>>> 34f6a9e9db589d205e84c71948f4063331eeb54f
 
 
 ReactDOM.render(
