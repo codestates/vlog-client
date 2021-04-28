c12da43efe161730fdaaa11163689dbd7e5a3dd4
mooon3356 <mooon3356@gmail.com>
2021-04-28 16:47:34 +0900

    updated Nav, MyPage

--- client/src/Pages/MyPage.tsx
+++ client/src/Pages/MyPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+// import PostLists from '~~~'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    border: 3px solid black;
+`
+
+const UserInfo = styled.div``
 
 function MyPage() {
     return (
-        <div>Mypage</div>
+        <Container>
+            {/* <TagsList /> */}
+            <UserInfo></UserInfo>
+            {/* <PostLists /> */}
+        </Container>
     )
 }
 
