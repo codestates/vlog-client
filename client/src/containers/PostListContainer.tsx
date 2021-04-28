7fe5235a15fc338e03118cae2431af98dca58914
Junrae Kim <jrk9204@gmail.com>
2021-04-28 23:43:02 +0900

    get data

--- client/src/containers/PostListContainer.tsx
+++ client/src/containers/PostListContainer.tsx
@@ -3,25 +3,37 @@ import {useSelector, useDispatch} from 'react-redux'
 import PostList from '../components/PostList'
 import {displayData} from '../modules/post'
 import { RootState } from '../modules';
+import axios from "axios";
+import {useEffect} from 'react';
+
 
 
 export default function PostListContainer() {
     
   
-   const state = useSelector((state:RootState) => (state.postReducer.data));
-   
-   console.log(state);
 
     
+   
+   const dispatch = useDispatch();
+   const state = useSelector((state:RootState) => (state.postReducer.data));
+
+   useEffect(() => {
+    axios.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyCk0LNtPlTSTmoV8-OwRpqY3z38q78v1aM')
+    .then(res =>{ 
+    
+        dispatch(displayData(res))
+    })
+    
+    },[]) 
+
     return (
         <div>
-
-        {state.map((element:any)=> {
-            return <PostList element={element} key={element.etag} />
-            // return  <img key = {element.id}  src={element.snippet.thumbnails.default.url}/>
-
-        })}
-       
+            {state === null ? <div>로딩중</div>: <div> 로딩성공 </div>}
+   
+        {state.map((element:any) => {
+            return  <img key = {element.id}  src={element.snippet.thumbnails.default.url}/>
+            }
+        )}
         </div>
     );
 }
--- client/src/modules/post.ts
+++ client/src/modules/post.ts
@@ -8,36 +8,30 @@ const DISPLAYDATA = 'post/DISPLAYDATA' as const;
 
 
 //만약 추가나 삭제 버튼을 눌렀을경우 데이터가 변경 
-export const displayData = ()=>({
+export const displayData = (data:any)=>({
     type : DISPLAYDATA,
+    data
 
 });
+
+
+
 
 //ReturnType<typeof__> 특정 함수의 반환값 추론해줌.
 // 모든 액션 리턴값 지정해줌.
 type PostAction =
     |ReturnType<typeof displayData>
 
-
-const apiKey = 'AIzaSyAzRlhVFB_RyvkvG-3JVdJEwcwwYMPzCjk'
-
-
-   
- const youtube =  function axios():any { 
-     return axios.get(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${apiKey}&part=snippet,contentDetails,statistics,status`)
-                .then((res) => res);
-    
-    }
-
-//reducer part
 type DataState = {
     data:any;
 }
 
 const init: DataState ={
-    data: youtube
-
+    data: null
 }
+   
+//reducer part
+
  
 
 export default function postReducer(state:DataState = init, action:PostAction):DataState{
@@ -45,6 +39,7 @@ export default function postReducer(state:DataState = init, action:PostAction):D
         case DISPLAYDATA:
             return {
             ...state,
+            data: [action.data]
         }
 
         default:
